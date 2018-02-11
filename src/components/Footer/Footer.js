e18325f3067342cad0aa8f54564141c6c43e85b8
Tais1990 <tais_kat@list.ru>
2018-02-11 19:54:37 +0500

    BEM

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,74 +4,30 @@ import svgTwitter 	from 'images/icons/tw.svg';
 import svgInstagram from 'images/icons/inst.svg';
 import svgFacebook 	from 'images/icons/fb.svg';
 
+import './Footer.scss';
+
 class Footer extends React.Component {
 	render() {
 		return (
-			/*
-			<div className = "home-container">
-				<div className ="copyright">
-					<div className = "home-table v-align">
-						<div className = "home-cell">
-							© 2017 Isina Inc.
-							<span className = "footer-links">
-								<a href="/terms-of-use" target="_self"> Terms of Use </a> /
-								<a href="/privacy-notice" target="_self"> Privacy Notice </a> /
-								<a href="/official-rules" target="_self"> Official Rules </a>
-							</span>
-						</div>
-					</div>
-					<div className = "footer__contacts">
-						<a href="tel:+18442347462" className ="footer__contacts-phone" target="_self">+1-844-234-7462</a>
-						<a href="mailto:info@isina.com" className ="footer__contacts-email" target="_self">info@isina.com</a>
-					</div>
-				</div>
-				<div className = "socialclass">
-					<ul className ="social">
-						<li className ="social__el">
-							<a href = "https://twitter.com/isinaofficial/" target="_self">
-								<Icon
-									className="HomePage__icon"
-									src={svgTwitter}
-								/>
-							</a>
-						</li>
-						<li className ="social__el">
-							<a href = "https://www.facebook.com/ISINAofficial/" target="_self">
-								<Icon
-									className="HomePage__icon"
-									src={svgFacebook}
-								/>
-							</a>
-						</li>
-						<li className ="social__el">
-							<a href = "https://www.instagram.com/isinaofficial/" target="_self">
-								<Icon
-									className="HomePage__icon"
-									src={svgInstagram}
-								/>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>*/
-			<footer className ="flaxConteiner">
-				<div className = "flex-item">
-					<div className = "copir">
-						<div className = "copiraiter">
+			<footer className ="footer">
+				<div className = "footer__info">
+					<div className = "footer__info__copirandref">
+						<div className = "footer__info__copirandref__copyright">
 							© 2017 Isina Inc.
 						</div>
-						<div className = "referense">
+						<div className = "footer__info__copirandref__referense">
 							<a href="/terms-of-use" target="_self"> Terms of Use </a> /
 							<a href="/privacy-notice" target="_self"> Privacy Notice </a> /
 							<a href="/official-rules" target="_self"> Official Rules </a>
 						</div>
 					</div>
-					<div className = "contact">
-						Контакты
+					<div className = "footer__info__contact">
+						<div className = "footer__info__contact__dev">+1-844-234-7462</div>
+						<div className = "footer__info__contact__dev">info@isina.com</div>
 					</div>
 				</div>
-				<div className = "flex-item">
-					<div>
+				<div className = "footer__socialReferences">
+					<div className = "footer__socialReferences__dev">
 						<a href = "https://twitter.com/isinaofficial/">
 							<Icon
 								className="HomePage__icon"
@@ -79,7 +35,7 @@ class Footer extends React.Component {
 							/>
 						</a>
 					</div>
-					<div>
+					<div  className = "footer__socialReferences__dev">
 						<a href = "https://www.facebook.com/ISINAofficial/">
 							<Icon
 								className="HomePage__icon"
@@ -87,7 +43,7 @@ class Footer extends React.Component {
 							/>
 						</a>
 					</div>
-					<div>
+					<div  className = "footer__socialReferences__dev">
 						<a href = "https://www.instagram.com/isinaofficial/">
 							<Icon
 								className="HomePage__icon"
