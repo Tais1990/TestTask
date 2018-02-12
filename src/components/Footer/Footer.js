ebf9af5ed343d96129de321b66ddae362b7b09a4
Tais1990 <tais_kat@list.ru>
2018-02-12 18:02:31 +0500

    scss recoding

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,42 +11,42 @@ class Footer extends React.Component {
 		return (
 			<footer className ="footer">
 				<div className = "footer__info">
-					<div className = "footer__info__copirandref">
-						<div className = "footer__info__copirandref__copyright">
+					<div className = "footer__copir-and-ref">
+						<div className = "footer__copyright">
 							© 2017 Isina Inc.
 						</div>
-						<div className = "footer__info__copirandref__referense">
+						<div className = "footer__referense">
 							<a href="/terms-of-use" target="_self"> Terms of Use </a> /
 							<a href="/privacy-notice" target="_self"> Privacy Notice </a> /
 							<a href="/official-rules" target="_self"> Official Rules </a>
 						</div>
 					</div>
-					<div className = "footer__info__contact">
-						<div className = "footer__info__contact__dev">+1-844-234-7462</div>
-						<div className = "footer__info__contact__dev">info@isina.com</div>
+					<div className = "footer__contacts">
+						<div className = "footer__contact">+1-844-234-7462</div>
+						<div className = "footer__contact">info@isina.com</div>
 					</div>
 				</div>
-				<div className = "footer__socialReferences">
-					<div className = "footer__socialReferences__dev">
+				<div className = "footer__social-references">
+					<div className = "footer__social-reference">
 						<a href = "https://twitter.com/isinaofficial/">
 							<Icon
-								className="HomePage__icon"
+								className="footer__social-icon"
 								src={svgTwitter}
 							/>
 						</a>
 					</div>
-					<div  className = "footer__socialReferences__dev">
+					<div  className = "footer__social-reference">
 						<a href = "https://www.facebook.com/ISINAofficial/">
 							<Icon
-								className="HomePage__icon"
+								className="footer__social-icon"
 								src={svgFacebook}
 							/>
 						</a>
 					</div>
-					<div  className = "footer__socialReferences__dev">
+					<div  className = "footer__social-reference">
 						<a href = "https://www.instagram.com/isinaofficial/">
 							<Icon
-								className="HomePage__icon"
+								className="footer__social-icon"
 								src={svgInstagram}
 							/>
 						</a>
